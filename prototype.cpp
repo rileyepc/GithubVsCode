87ec28ee65f78bb000630647e99d19ad2aca3ac4
rileyepc <riley.turner@epcpower.com>
2022-04-20 10:11:25 -0700

    function add

---
+++ prototype.cpp
@@ -0,0 +1,27 @@
+// using function definition after main() function
+// function prototype is declared before main()
+
+#include <iostream>
+//test line
+
+using namespace std;
+
+// function prototype
+int add(int, int);
+
+int main() {
+    int sum;
+
+    // calling the function and storing
+    // the returned value in sum
+    sum = add(100, 78);
+
+    cout << "100 + 78 = " << sum << endl;
+
+    return 0;
+}
+
+// function definition
+int add(int a, int b) {
+    return (a + b);
+}
